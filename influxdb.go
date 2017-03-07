21b3b5e1bd17b7affffcf2ac5a740fa3dd5d001d
LeslieZheng <zhengwei3@douyu.tv>
2017-03-07 19:41:36 +0800

    convert old client to v2.client

--- influxdb.go
+++ influxdb.go
@@ -3,10 +3,11 @@ package influxdb
 import (
 	"fmt"
 	"log"
-	uurl "net/url"
+	"net/url"
 	"time"
+	"errors"
 
-	"github.com/influxdata/influxdb/client"
+	"github.com/influxdata/influxdb/client/v2"
 	"github.com/rcrowley/go-metrics"
 )
 
@@ -14,32 +15,26 @@ type reporter struct {
 	reg      metrics.Registry
 	interval time.Duration
 
-	url      uurl.URL
+	addr     string
 	database string
 	username string
 	password string
 	tags     map[string]string
 
-	client *client.Client
+	client client.Client
 }
 
 // InfluxDB starts a InfluxDB reporter which will post the metrics from the given registry at each d interval.
-func InfluxDB(r metrics.Registry, d time.Duration, url, database, username, password string) {
-	InfluxDBWithTags(r, d, url, database, username, password, nil)
+func InfluxDB(r metrics.Registry, d time.Duration, addr, database, username, password string) {
+	InfluxDBWithTags(r, d, addr, database, username, password, nil)
 }
 
 // InfluxDBWithTags starts a InfluxDB reporter which will post the metrics from the given registry at each d interval with the specified tags
-func InfluxDBWithTags(r metrics.Registry, d time.Duration, url, database, username, password string, tags map[string]string) {
-	u, err := uurl.Parse(url)
-	if err != nil {
-		log.Printf("unable to parse InfluxDB url %s. err=%v", url, err)
-		return
-	}
-
+func InfluxDBWithTags(r metrics.Registry, d time.Duration, addr, database, username, password string, tags map[string]string) {
 	rep := &reporter{
 		reg:      r,
 		interval: d,
-		url:      *u,
+		addr:     addr,
 		database: database,
 		username: username,
 		password: password,
@@ -54,11 +49,27 @@ func InfluxDBWithTags(r metrics.Registry, d time.Duration, url, database, userna
 }
 
 func (r *reporter) makeClient() (err error) {
-	r.client, err = client.NewClient(client.Config{
-		URL:      r.url,
-		Username: r.username,
-		Password: r.password,
-	})
+	url, err := url.Parse(r.addr)
+	if err != nil {
+		log.Printf("unable to parse InfluxDB address %s. err=%v", r.addr, err)
+		return
+	}
+
+	switch url.Scheme {
+	case "http":
+		r.client, err = client.NewHTTPClient(client.HTTPConfig{
+			Addr:     r.addr,
+			Username: r.username,
+			Password: r.password,
+		})
+	case "udp":
+		r.client, err = client.NewUDPClient(client.UDPConfig{
+			Addr:     url.Host,
+			PayloadSize: 1024,
+		})
+	default:
+		err = errors.New("invalid scheme")
+	}
 
 	return
 }
@@ -74,7 +85,7 @@ func (r *reporter) run() {
 				log.Printf("unable to send metrics to InfluxDB. err=%v", err)
 			}
 		case <-pingTicker:
-			_, _, err := r.client.Ping()
+			_, _, err := r.client.Ping(1 * time.Second)
 			if err != nil {
 				log.Printf("got error while sending a ping to InfluxDB, trying to recreate client. err=%v", err)
 
@@ -87,7 +98,12 @@ func (r *reporter) run() {
 }
 
 func (r *reporter) send() error {
-	var pts []client.Point
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:r.database,
+	})
+	if err != nil {
+		return err
+	}
 
 	r.reg.Each(func(name string, i interface{}) {
 		now := time.Now()
@@ -95,104 +111,80 @@ func (r *reporter) send() error {
 		switch metric := i.(type) {
 		case metrics.Counter:
 			ms := metric.Snapshot()
-			pts = append(pts, client.Point{
-				Measurement: fmt.Sprintf("%s.count", name),
-				Tags:        r.tags,
-				Fields: map[string]interface{}{
-					"value": ms.Count(),
-				},
-				Time: now,
-			})
+			fields := map[string]interface{}{
+				"value": ms.Count(),
+			}
+			pt, _ := client.NewPoint(fmt.Sprintf("%s.count", name), r.tags, fields, now)
+			bp.AddPoint(pt)
 		case metrics.Gauge:
 			ms := metric.Snapshot()
-			pts = append(pts, client.Point{
-				Measurement: fmt.Sprintf("%s.gauge", name),
-				Tags:        r.tags,
-				Fields: map[string]interface{}{
-					"value": ms.Value(),
-				},
-				Time: now,
-			})
+			fields := map[string]interface{}{
+				"value": ms.Value(),
+			}
+			pt, _ := client.NewPoint(fmt.Sprintf("%s.gauge", name), r.tags, fields, now)
+			bp.AddPoint(pt)
 		case metrics.GaugeFloat64:
 			ms := metric.Snapshot()
-			pts = append(pts, client.Point{
-				Measurement: fmt.Sprintf("%s.gauge", name),
-				Tags:        r.tags,
-				Fields: map[string]interface{}{
-					"value": ms.Value(),
-				},
-				Time: now,
-			})
+			fields := map[string]interface{}{
+				"value": ms.Value(),
+			}
+			pt, _ := client.NewPoint(fmt.Sprintf("%s.gauge", name), r.tags, fields, now)
+			bp.AddPoint(pt)
 		case metrics.Histogram:
 			ms := metric.Snapshot()
 			ps := ms.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999, 0.9999})
-			pts = append(pts, client.Point{
-				Measurement: fmt.Sprintf("%s.histogram", name),
-				Tags:        r.tags,
-				Fields: map[string]interface{}{
-					"count":    ms.Count(),
-					"max":      ms.Max(),
-					"mean":     ms.Mean(),
-					"min":      ms.Min(),
-					"stddev":   ms.StdDev(),
-					"variance": ms.Variance(),
-					"p50":      ps[0],
-					"p75":      ps[1],
-					"p95":      ps[2],
-					"p99":      ps[3],
-					"p999":     ps[4],
-					"p9999":    ps[5],
-				},
-				Time: now,
-			})
+			fields := map[string]interface{}{
+				"count":    ms.Count(),
+				"max":      ms.Max(),
+				"mean":     ms.Mean(),
+				"min":      ms.Min(),
+				"stddev":   ms.StdDev(),
+				"variance": ms.Variance(),
+				"p50":      ps[0],
+				"p75":      ps[1],
+				"p95":      ps[2],
+				"p99":      ps[3],
+				"p999":     ps[4],
+				"p9999":    ps[5],
+			}
+			pt, _ := client.NewPoint(fmt.Sprintf("%s.histogram", name), r.tags, fields, now)
+			bp.AddPoint(pt)
 		case metrics.Meter:
 			ms := metric.Snapshot()
-			pts = append(pts, client.Point{
-				Measurement: fmt.Sprintf("%s.meter", name),
-				Tags:        r.tags,
-				Fields: map[string]interface{}{
-					"count": ms.Count(),
-					"m1":    ms.Rate1(),
-					"m5":    ms.Rate5(),
-					"m15":   ms.Rate15(),
-					"mean":  ms.RateMean(),
-				},
-				Time: now,
-			})
+			fields := map[string]interface{}{
+				"count": ms.Count(),
+				"m1":    ms.Rate1(),
+				"m5":    ms.Rate5(),
+				"m15":   ms.Rate15(),
+				"mean":  ms.RateMean(),
+			}
+			pt, _ := client.NewPoint(fmt.Sprintf("%s.meter", name), r.tags, fields, now)
+			bp.AddPoint(pt)
 		case metrics.Timer:
 			ms := metric.Snapshot()
 			ps := ms.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999, 0.9999})
-			pts = append(pts, client.Point{
-				Measurement: fmt.Sprintf("%s.timer", name),
-				Tags:        r.tags,
-				Fields: map[string]interface{}{
-					"count":    ms.Count(),
-					"max":      ms.Max(),
-					"mean":     ms.Mean(),
-					"min":      ms.Min(),
-					"stddev":   ms.StdDev(),
-					"variance": ms.Variance(),
-					"p50":      ps[0],
-					"p75":      ps[1],
-					"p95":      ps[2],
-					"p99":      ps[3],
-					"p999":     ps[4],
-					"p9999":    ps[5],
-					"m1":       ms.Rate1(),
-					"m5":       ms.Rate5(),
-					"m15":      ms.Rate15(),
-					"meanrate": ms.RateMean(),
-				},
-				Time: now,
-			})
+			fields := map[string]interface{}{
+				"count":    ms.Count(),
+				"max":      ms.Max(),
+				"mean":     ms.Mean(),
+				"min":      ms.Min(),
+				"stddev":   ms.StdDev(),
+				"variance": ms.Variance(),
+				"p50":      ps[0],
+				"p75":      ps[1],
+				"p95":      ps[2],
+				"p99":      ps[3],
+				"p999":     ps[4],
+				"p9999":    ps[5],
+				"m1":       ms.Rate1(),
+				"m5":       ms.Rate5(),
+				"m15":      ms.Rate15(),
+				"meanrate": ms.RateMean(),
+			}
+			pt, _ := client.NewPoint(fmt.Sprintf("%s.timer", name), r.tags, fields, now)
+			bp.AddPoint(pt)
 		}
 	})
-
-	bps := client.BatchPoints{
-		Points:   pts,
-		Database: r.database,
-	}
-
-	_, err := r.client.Write(bps)
-	return err
+	
+	return r.client.Write(bp)
 }
